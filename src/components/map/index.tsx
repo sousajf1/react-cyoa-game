a6fc4368c177c8d4f657f9a35850c8e4b10d7f8a
sousajo <sousajo@pop-os.localdomain>
2021-02-24 19:42:22 +0100

    [](){}: add objects to map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,12 +1,56 @@
 import React from "react";
+import './styles.css';
 
-export const Map = () => {
+
+interface Props1{
+    tiles: number[][];
+}
+interface Props2{
+    tiles: number[];
+}
+interface Props3{
+    value: number;
+}
+
+export const getTileSprite = (value: number) => {
+    switch (value as any){
+        case 0:
+            return 'grass';
+        case 1:
+            return 'rock';
+    }
+}
+
+
+
+export const MapTile: React.FC<Props3> =({value}) => {
+    const val = value;
+    return <div className={`tile ${getTileSprite(val)}`}
+    style={{
+        height: "33px",
+        width: "32px",
+    }}/>
+}
+
+export const MapRow: React.FC<Props2> = ({tiles}) => {
+    return <div className="row">
+        {
+            tiles.map(tile => <MapTile value={tile}/>)
+        }
+    </div>
+}
+
+export const Map: React.FC<Props1> = ({tiles}) => {
     return <div
         style={{
-            position:"absolute",
+            position: "relative",
             width: "800px",
             height: "400px",
+            margin: "20px auto",
             backgroundColor: "#fff59d",
-            border: "4px solid white",
-        }}/>
+        }}>
+        {
+            tiles.map(row => <MapRow tiles={row} />)
+        }
+    </div>
 };
--- src/components/world/index.tsx
+++ src/components/world/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import {Map} from "../map";
 import {Player} from "../player";
+import {tiles} from "../../config/maps";
 
 export const World = () => {
-    return (<div
+    // @ts-ignore
+    return <div
             style={{
                 position: "relative",
                 width: "800px",
                 height: "400px",
                 margin: "20px auto"
             }}>
-           <Map />
+
+           <Map tiles={tiles} />
            <Player />
         </div>
-    )
 };
